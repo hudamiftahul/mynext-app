26821d1c6c76b375418b94106016f16006d78914
huda <anehuda1601@gmail.com>
2026-03-30 11:02:32 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,54 @@
+import Link from "next/link";
+
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+}
+
+export default async function Home() {
+  const users = await getUsers();
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store"
+  });
+
+  return (
+    <div>
+      <div className="bg-black text-white p-10">
+        <h1 className="text-3xl font-bold">Huda App 🚀</h1>
+    </div>
+      <p>Ini project pertama Next.js</p>
+      <Link href="/about">Ke About</Link>
+      <div className="max-w-4xl mx-auto p-5">
+        <h1 className="text-2xl font-bold mb-4">Daftar User</h1>
+        <input
+          type="text"
+          placeholder="Cari user..."
+          className="border p-2 rounded w-full mb-4"
+        />
+        <table className="w-full border border-gray-300 mt-5">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className="p-3 text-left">Nama</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Kota</th>
+              <th className="p-3 text-left">No Telp</th>
+              <th className="p-3 text-left">Nama Perusahaan</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id } className="border-t hover:bg-gray-100">
+                <td className="p-3">{user.name}</td>
+                <td className="p-3">{user.email}</td>
+                <td className="p-3">{user.address.city}</td>
+                <td className="p-3">{user.phone}</td>
+                <td className="p-3">{user.company.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
